0c787e107fab9b1b47ef8f990be9684bb0cb19db
edsoncgjunior <ecgjunior8@gmail.com>
2022-10-13 16:20:30 -0300

    first commit

---
+++ __tests__/TC02.js
@@ -0,0 +1,31 @@
+//Using data driven and page objects
+
+Login = require('../__tests__/pageObjects/login')
+Data = require('../data/data.json')
+
+describe('Post', () => {
+
+    const login = new Login()
+
+    beforeAll(async () => {
+        await page.goto(Data.url)
+    })
+
+    test('Sign In', async () => {
+        const title = await page.title()
+        expect(title).toBe('Conduit')
+
+        const email = await login.email()
+        await email.fill(Data.email)
+        await email.press('Tab')
+        await login.password_fill(Data.password)
+        await login.signInButton_click()
+
+        const html = await page.innerHTML('.feed-toggle')
+        expect(html).toMatch('Your Feed')
+    })
+
+    afterAll(async () => {
+        await browser.close()
+    })
+})
